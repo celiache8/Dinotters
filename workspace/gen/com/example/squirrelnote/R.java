a2a6b87f64a08e976b838d931397f1e6421ce7a6
Celia Chen <celiachen0822@gmail.com>
2014-03-20 15:45:09 -0700

    Yess!

--- workspace/gen/com/example/squirrelnote/R.java
+++ workspace/gen/com/example/squirrelnote/R.java
@@ -11,21 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int add=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int addImageButton=0x7f070001;
+        public static final int checklist=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int note=0x7f070003;
+        public static final int relative=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list_main=0x7f030001;
+        public static final int note_main=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int menu_settings=0x7f040001;
     }
     public static final class style {
         /** 
--- workspace/src/com/example/squirrelnote/MainActivity.java
+++ workspace/src/com/example/squirrelnote/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.squirrelnote;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+			}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,5 +23,42 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	// After the add button is pressed,
+	// allow user to choose either adding a note or a list
+	// use a dialog
 
+	public void showDialog(View v){
+		final CharSequence[] items = {"CheckList", "Note"};
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Pick a type");
+		builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {}
+		});
+		
+		builder.setSingleChoiceItems(items,-1, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+				if("Checklist".equals(items[which]))
+				{
+					Intent intentCheck = new Intent(MainActivity.this, ListActivity.class);
+					MainActivity.this.startActivity(intentCheck);
+					Log.i("Content "," CheckList Layout ");
+				}
+				else if("Note".equals(items[which]))
+				{
+					Intent intentNote = new Intent(MainActivity.this, NoteActivity.class);
+					MainActivity.this.startActivity(intentNote);
+					Log.i("Content "," Note Layout ");
+				}
+				
+				
+			}
+		});
+    	builder.show();
+	}
 }
