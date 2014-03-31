905351dc8868257fc7e571b6c30fb1a165e89c20
Celia Chen <celiachen0822@gmail.com>
2014-03-31 16:40:22 -0700

    AHHH

--- workspace/src/com/example/squirrelnote/NoteActivity.java
+++ workspace/src/com/example/squirrelnote/NoteActivity.java
@@ -3,7 +3,7 @@ package com.example.squirrelnote;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class NoteActivity extends Activity{
+public class NoteActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
