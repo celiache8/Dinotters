d713ebdd5858a6f1fc39af273ffb7b7bbfc5fd76
Celia Chen <celiachen0822@gmail.com>
2014-03-31 17:41:19 -0700

    yay!

--- workspace/src/com/example/squirrelnote/ChecklistActivity.java
+++ workspace/src/com/example/squirrelnote/ChecklistActivity.java
@@ -2,6 +2,8 @@ package com.example.squirrelnote;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class ChecklistActivity extends ListActivity{
 	@Override
@@ -9,5 +11,18 @@ public class ChecklistActivity extends ListActivity{
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.list_main);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+	  switch (item.getItemId())
+	  {
+	  case android.R.id.home:
+		  NavUtils.navigateUpFromSameTask(this);
+		  return true;
+	  default:
+		  return super.onOptionsItemSelected(item);
+	  }
 	}
 }
